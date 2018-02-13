f08102a21e38690b27cfe949cbddd3123ec4c8f4
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-02-12 20:22:25 -0800

    Sabotage

--- code.py
+++ code.py
@@ -8,8 +8,9 @@ def image_detect():
 	print('detects an image!!!!')		
 	pass
 
+# AHAHAHAHAA SABOTAGE!!!!
 # aahhh!
-# I;m made of antimatter muahahaha
+# I;m made of antimatter
 
 
 # WORKING ON THIS SIMULTANEOUSLY
